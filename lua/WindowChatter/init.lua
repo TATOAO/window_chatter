79d63cf6eddcde795249605627b4600d225de5ec
TATOAO <liangwentaoss@126.com>
2024-05-09 09:33:56 +0800

    color issue not fixed

--- lua/WindowChatter/init.lua
+++ lua/WindowChatter/init.lua
@@ -103,14 +103,43 @@ local function send_visual_selection_to_window()
 end
 
 
--- Function to update the highlighting of the text region
+local function on_lines(_, buf, _, firstline, lastline, new_lastline, _)
+    for i, region in ipairs(masked_regions) do
+        if region.start_line <= firstline and region.end_line >= lastline then
+            -- Adjust the region end line based on the difference in line changes
+            local line_diff = new_lastline - lastline
+            region.end_line = region.end_line + line_diff
+
+            -- Fetch the new lines within the region
+            local lines = vim.fn.getline(region.start_line, region.end_line)
+
+            -- Update the floating window with new content
+            create_or_update_window(i, lines)
+
+            -- Update highlighting
+            update_highlight(i, region.start_line, 0, region.end_line, #lines[#lines])
+        end
+    end
+end
+
+-- Attach the listener to each buffer when needed
+local function attach_buffer()
+    local buf = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_attach(buf, false, {
+        on_lines = on_lines
+    })
+end
+
+
+
+-- Updated highlight function, assumes global access to mask_ns_id_list
 local function update_highlight(index, start_line, start_col, end_line, end_col)
     local highlight_group = "MaskHighlight" .. index
     vim.api.nvim_buf_clear_namespace(0, mask_ns_id_list[index], 0, -1)
 
-    vim.api.nvim_buf_add_highlight(0, mask_ns_id_list[index], highlight_group, start_line - 1, start_col - 1, end_col)
-    for i = start_line + 1, end_line - 1 do
-        vim.api.nvim_buf_add_highlight(0, mask_ns_id_list[index], highlight_group, i - 1, 0, -1)
+    vim.api.nvim_buf_add_highlight(0, mask_ns_id_list[index], highlight_group, start_line - 1, start_col, -1)
+    for line = start_line + 1, end_line - 1 do
+        vim.api.nvim_buf_add_highlight(0, mask_ns_id_list[index], highlight_group, line - 1, 0, -1)
     end
     vim.api.nvim_buf_add_highlight(0, mask_ns_id_list[index], highlight_group, end_line - 1, 0, end_col)
 end
@@ -118,33 +147,51 @@ end
 
 -- Function to update the floating window and the highlight when the text changes
 local function update_window_on_change()
+    -- This function might be simplified or even unnecessary if `on_lines` handles all updates
     for i, region in ipairs(masked_regions) do
-        local start_line, start_col, end_line, end_col = region.start_line, region.start_col, region.end_line, region.end_col
-        local lines = vim.fn.getline(start_line, end_line)
-
-        lines[1] = string.sub(lines[1], start_col)
-        lines[#lines] = string.sub(lines[#lines], 1, end_col)
-
-        -- Update window content
+        local lines = vim.fn.getline(region.start_line, region.end_line)
         create_or_update_window(i, lines)
-
-        -- Recalculate end_line and end_col based on changes
-        end_line = start_line + #lines - 1
-        if #lines > 1 then
-            end_col = #lines[#lines]
-        else
-            end_col = start_col + #lines[1]
-        end
-
-        -- Update the highlight region
-        update_highlight(i, start_line, start_col, end_line, end_col)
-
-        -- Update region data to reflect new end_line and end_col
-        region.end_line = end_line
-        region.end_col = end_col
+        update_highlight(i, region.start_line, 0, region.end_line, #lines[#lines])
     end
 end
 
+
+
+-- local function update_window_on_change()
+--     for i, region in ipairs(masked_regions) do
+--         local start_line, start_col, original_end_line, original_end_col = region.start_line, region.start_col, region.end_line, region.end_col
+--         local lines = vim.fn.getline(start_line, original_end_line)
+--
+--         if start_line == original_end_line then
+--             lines[1] = string.sub(lines[1], start_col, original_end_col)
+--         else
+--             lines[1] = string.sub(lines[1], start_col)
+--             lines[#lines] = string.sub(lines[#lines], 1, original_end_col)
+--         end
+--
+--         -- Update window content
+--         create_or_update_window(i, lines)
+--
+--         -- Determine new end_line based on the number of new lines added within the original region
+--         local new_end_line = start_line + #lines - 1
+--
+--         -- Adjust end line if new lines have been added within the highlighted area
+--         local current_line_count = original_end_line - start_line + 1
+--         local new_line_count = new_end_line - start_line + 1
+--         if new_line_count > current_line_count then
+--             region.end_line = region.end_line + (new_line_count - current_line_count)
+--         elseif new_line_count < current_line_count then
+--             region.end_line = region.end_line - (current_line_count - new_line_count)
+--         end
+--
+--         -- Recalculate end_col of the last line if the last line is edited
+--         region.end_col = #vim.fn.getline(region.end_line)
+--
+--         -- Update the highlight region
+--         update_highlight(i, start_line, 0, region.end_line, region.end_col)
+--     end
+-- end
+--
 
 vim.cmd([[
     augroup UpdateFloatingWindow
